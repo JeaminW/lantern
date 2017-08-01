5d0bee33ee9d7418bbc33d9218d9113d06b587a8
Jeamin, Wong <wjm231013@gmail.com>
2017-08-01 10:49:27 +0800

    Add 'saddr' flag to custom socket proxy address

--- src/github.com/getlantern/flashlight/main/flags.go
+++ src/github.com/getlantern/flashlight/main/flags.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	addr                   = flag.String("addr", "", "ip:port on which to listen for requests. When running as a client proxy, we'll listen with http, when running as a server proxy we'll listen with https (required)")
+	saddr                  = flag.String("saddr", "", "ip:port on which to listen for socket requests.")
 	configdir              = flag.String("configdir", "", "directory in which to store configuration, including flashlight.yaml (defaults to current directory)")
 	cloudconfig            = flag.String("cloudconfig", "", "optional http(s) URL to a cloud-based source for configuration updates")
 	cloudconfigca          = flag.String("cloudconfigca", "", "optional PEM encoded certificate used to verify TLS connections to fetch cloudconfig")
